42afb760ebe6a73fb600489345674b8ef4a6f6a3
Shrishti Gupta <gupta.shrishti.0111@gmail.com>
2025-03-09 01:16:55 +0530

    Changes

--- src/main/java/com/resume/backend/controller/ResumeController.java
+++ src/main/java/com/resume/backend/controller/ResumeController.java
@@ -2,11 +2,12 @@ package com.resume.backend.controller;
 
 import com.resume.backend.service.ResumeRequest;
 import com.resume.backend.service.ResumeService;
-import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -20,8 +21,8 @@ public class ResumeController {
     }
 
     @PostMapping("generate")
-    public ResponseEntity<JSONObject> getResumeData(@RequestBody ResumeRequest resumeRequest) throws IOException {
-        JSONObject response = resumeService.generateResumeResponse(resumeRequest.userDescription());
-        return ResponseEntity.ok(response);
+    public ResponseEntity<Map<String,Object>> getResumeData(@RequestBody ResumeRequest resumeRequest) throws IOException {
+        Map<String, Object> stringObjectMap = resumeService.generateResumeResponse(resumeRequest.userDescription());
+        return new ResponseEntity<>(stringObjectMap, HttpStatus.OK);
     }
 }
